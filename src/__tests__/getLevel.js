cedc41c330eb0c055fa90e6861e5d7810df7b7df
Maria Tomilova <m.tomilova2802@gmail.com>
2026-07-14 20:40:30 +0300

    first commit

---
+++ src/__tests__/getLevel.js
@@ -0,0 +1,40 @@
+import getLevel from "../getLevel";
+import fetchData from "../fetchData";
+
+jest.mock('../fetchData');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should return level when response status is "ok"', () => {
+    const mockResponse = {
+      status: 'ok',
+      level: 42,
+    };
+    fetchData.mockReturnValue(mockResponse);
+    
+    const result = getLevel(1);
+
+    expect(result).toBe('Ваш текущий уровень: 42');
+});
+
+test('should return level when response status is not "ok"', () => {
+    const mockResponse = {
+      status: 'null',
+      level: 42,
+    };
+    fetchData.mockReturnValue(mockResponse);
+    
+    const result = getLevel(1);
+
+    expect(result).toBe('Информация об уровне временно недоступна');
+});
+
+test('should errors thrown by fetchData', () => {
+    fetchData.mockImplementation(() => {
+        throw new Error('Mock this!');
+    });
+
+    expect(() => getLevel(1)).toThrow('Mock this!');
+});
